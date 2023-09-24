77fd8e4fc267bfa5cee4a6ceb939ba72a03a190b
Nayase <black815814@gmail.com>
2023-09-24 17:50:07 +0900

    modified

--- onlyXT2Copyer5_delete.py
+++ onlyXT2Copyer5_delete.py
@@ -1,27 +1,30 @@
+# -*- coding: utf-8 -*-
+
 import os
 import os.path
 import datetime
 import shutil
 
-
-
 def main():
 
     #元のファイルのパス
-    dirpath = "E:/DCIM"
+    if os.path.exists("/Volumes/NO NAME/DCIM"):
+        dirpath = "/Volumes/NO NAME/DCIM/"
+    if os.path.exists("/Volumes/SD CARD01/DCIM"):
+        dirpath = "/Volumes/SD CARD01/DCIM/"
+    if os.path.exists("/Volumes/Untitled/DCIM"):
+        dirpath = "/Volumes/Untitled/DCIM/"    
     dirfiles = os.listdir(dirpath)
     
     #コピー先のファイルのパス
-    copypath = "C:/Users/Owner/Pictures/Camera Roll/2021"
-
-    
+    copypath = "/Users/owner/Pictures/Camera Roll"
 
     print("コピーしています。")
 
     #ファイルのコピーを行う
     for j in dirfiles:
 
-        path = "E:/DCIM/" + j
+        path = dirpath + j
         files = os.listdir(path)
 
         for i in files:
@@ -34,9 +37,14 @@ def main():
 
             yeartime = dtime.strftime('%Y')
             monthtime = dtime.strftime('%m')
-
-            if not os.path.exists(copypath + "/" + yeartime + "-" + monthtime):
-                os.mkdir(copypath + "/" + yeartime + "-" + monthtime)
+            
+            if not os.path.exists(copypath + "/" + yeartime):
+                os.mkdir(copypath + "/" + yeartime)
+                
+            copypath_prime = copypath + "/" + yeartime
+            
+            if not os.path.exists(copypath_prime + "/" + yeartime + "-" + monthtime):
+                os.mkdir(copypath_prime + "/" + yeartime + "-" + monthtime)
 
             YMtime = yeartime + "-" + monthtime
 
@@ -44,44 +52,43 @@ def main():
             #dtimeをdatetimeからstrに型変換する
 
             tstr = dtime.strftime('%Y-%m-%d')
-            copypath2 = copypath + "/" + YMtime
+            copypath2 = copypath_prime + "/" + YMtime
             #print(tstr)
 
             if not os.path.exists(copypath2 + "/" + tstr) :
                 #ディレクトリを作成、重複する場合はスキップする。
                 os.mkdir(copypath2 + "/" + tstr)
+                os.mkdir(copypath2 + "/" + tstr + '/MOV')
                 os.mkdir(copypath2 + "/" + tstr + '/RAW')
                 os.mkdir(copypath2 + "/" + tstr + '/JPEG')
-
-            
-            
+                
+  
             #ファイルの拡張子の判断
             root, ext = os.path.splitext(path + '/' + i)
-
-            if ext == ".RAF" :
+            RAWjudge = ext == ".RAF" or ext == ".CR2" or ext == ".CRW" or ext == ".NEF" or ext == ".NRW" or ext == ".ARW" or ext == ".SR2" or ext == ".SRF" or ext == ".ORF" or ext == ".RW2" or ext == ".RAF" or ext == ".DNG" or ext == ".PEF" or ext == ".BAY" or ext == ".RWL" or ext == ".X3F"
+            if RAWjudge:
                 #SDカードから作成したフォルダにファイルをコピー
                 #copy2で作成日などのメタ情報もコピー
                 shutil.copy2(filepath, copypath2 + "/"  + tstr + '/RAW')
             #jpegの表記ゆれに対応
             elif ext == ".JPG" or ext == ".jpg" or ext == ".JPEG" or ext == ".jpeg" :
                 shutil.copy2(filepath, copypath2 + "/"  + tstr + '/JPEG')
-                
-    #ファイルの削除を行う
-    for j in dirfiles:
+            elif ext == ".MOV" or ext == ".MP4" :
+                shutil.copy2(filepath, copypath2 + "/"  + tstr + '/MOV')
 
-        path = "E:/DCIM/" + j
-        files = os.listdir(path)
+             
+        # #ファイルの削除を行う
+        for j in dirfiles:
 
-        for i in files:
-            #1ファイルずつ削除を行う。
+            path = dirpath + j
+            files = os.listdir(path)
 
-            filepath = path + "/" + i
+            for i in files:
+                #1ファイルずつ削除を行う。
 
-            os.remove(filepath)
-         
-    
-            
-    
+                filepath = path + "/" + i
+
+                os.remove(filepath) 
 
     print("コピーが完了しました")
     
